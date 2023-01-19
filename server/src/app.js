7337a54ab9b12976f4a1df473ffb3b2c44fc869a
nafis <nafu22@gmail.com>
2023-01-18 19:57:42 -0500

    added new command to read front from server directly

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./Routes/planets/plantes.router");
+const path = require("path");
 const app = express(); // fancy listener funciton
 app.use(cors({
     origin:"http://localhost:3000"
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"..","public")))
 app.use(planetsRouter);
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+
+});
 module.exports = app;
